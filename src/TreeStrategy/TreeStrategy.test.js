b6159803aa5ebd3187bbe8384da42acc1e21f6ad
tothricsaj <ricktothdevelop@gmail.com>
2021-06-30 20:07:25 +0200

    (test): tested TreeStrategy inorder

--- src/TreeStrategy/TreeStrategy.test.js
+++ src/TreeStrategy/TreeStrategy.test.js
@@ -18,4 +18,35 @@ describe('TreeStrategy features', () => {
 
 		expect(actual).toBe(expected);
 	});
+
+	it('should act like BST inorder', () => {
+		treeStrategy.strategy = new BinarySearchTree();
+
+      //       8
+      //     /   \
+      //    3     10
+      //   / \   /  \
+      //  1  4  9   12 
+
+      let dataArr = [];
+      const expectedArr = [1,3,4,8,9,10,12];
+
+      const cb = (dataArr) => {
+        return (data) => {
+          dataArr.push(data);
+        }
+      }
+
+      treeStrategy.strategy.insert(8);
+      treeStrategy.strategy.insert(10);
+      treeStrategy.strategy.insert(3);
+      treeStrategy.strategy.insert(4);
+      treeStrategy.strategy.insert(1);
+      treeStrategy.strategy.insert(12);
+      treeStrategy.strategy.insert(9);
+
+      treeStrategy.strategy.inorder(cb(dataArr));
+
+			expect(dataArr).toEqual(expectedArr);
+	});
 });
